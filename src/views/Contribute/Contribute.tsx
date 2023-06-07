78b35be0039561059019166a329576afacd5f6d7
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-06-07 13:14:08 +0700

    feat: contribute page

---
+++ src/views/Contribute/Contribute.tsx
@@ -0,0 +1,16 @@
+const Contribute = () => {
+	return (
+		<>
+			<div className='font-serif text-2xl font-medium'>Contribute</div>
+
+			<div className='mt-2 text-neutral-gray'>
+				<div>
+					Hi my fellow engineers, find this project interesting? Feel free to
+					contribute to this project by submitting a pull request to the
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Contribute;
