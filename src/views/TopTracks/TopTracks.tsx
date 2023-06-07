b3c1928f5931a31e3c3d62f4915bfa18d1ecc208
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-06-07 23:16:14 +0700

    feat: top track wording

--- src/views/TopTracks/TopTracks.tsx
+++ src/views/TopTracks/TopTracks.tsx
@@ -1,17 +1,25 @@
 import { Outlet } from "react-router-dom";
 
 import NavLink from "../../components/NavLink/NavLink";
+// import AppButton from "../../components/AppButton/AppButton";
 
 const TopTracks = () => {
 	return (
 		<>
 			<div className='sticky top-3 z-50 bg-black pb-4 pt-0 sm:top-[72px] sm:pt-0'>
-				<div>
-					<div className='text-lg font-medium'>My Top Tracks</div>
-					<div className='text-sm text-neutral-gray'>
-						Don't forget to share the moment
+				<div className='flex items-center justify-between'>
+					<div>
+						<div className='text-lg font-medium'>My Top Tracks</div>
+						<div className='text-sm text-neutral-gray'>
+							Here's your top 10 most-played tracks
+						</div>
 					</div>
+
+					{/* <div>
+						<AppButton>Add to playlist</AppButton>
+					</div> */}
 				</div>
+
 				<div className='hide-scroll-bar mt-3 flex w-full overflow-scroll sm:mt-4'>
 					<NavLink path='/top-tracks/short-term' name='4 Weeks' />
 					<NavLink path='/top-tracks/medium-term' name='6 Months' />
