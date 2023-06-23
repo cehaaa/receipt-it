a5b25059025228bea8cb96e32c5815c27058bb41
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-06-24 03:46:39 +0700

    fix: fix if not authenticated

--- src/views/Receipts/Receipts.tsx
+++ src/views/Receipts/Receipts.tsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Outlet } from "react-router-dom";
 
 import NavLink from "../../components/NavLink/NavLink";
 
+import AppContext from "../../context/AppContext";
+
 const Receipts = () => {
+	const { isAuthenticated } = useContext(AppContext);
+
 	const navigate = useNavigate();
 
 	useEffect(() => {
+		if (!isAuthenticated) {
+			navigate("/not-connected");
+			return;
+		}
+
 		navigate("/receipts/short-term");
-	}, []);
+	}, [isAuthenticated, navigate]);
+
+	if (!isAuthenticated) return null;
 
 	return (
-		<div className='flex flex-col space-x-10 sm:flex-row'>
+		<div className='flex flex-col justify-between sm:flex-row'>
 			<div className='flex flex-row sm:flex-col sm:space-y-5'>
 				<NavLink path='/receipts/short-term' name='4 Weeks' />
 				<NavLink path='/receipts/medium-term' name='6 Months' />
